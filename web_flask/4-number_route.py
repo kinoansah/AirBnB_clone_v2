3d33da633fea22883cf2c46a6d02fefa82760c0e
kinoansah <kinoansah@gmail.com>
2023-10-21 09:55:28 -0700

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
 """
 Flask web application
-@authors: Samuel Atiemo & Mahmoud Khairi.
 """
+
 from flask import Flask
+from flask import escape
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Start a basic Flask web application"""
-    return 'Hello HBNB!'
+def hello_hbnb():
+    """Display 'Hello HBNB!' on the root route."""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Adding a specific route /hbnb"""
-    return 'HBNB'
+    """Display 'HBNB' on the '/hbnb' route."""
+    return "HBNB"
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
-def c_text(text=None):
-    """Dynamic inputed text: C + replace _ for space and show text"""
-    return "C {}".format(text.replace('_', ' '))
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Display 'C' followed by the value of the text variable."""
+    return "C " + escape(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/<string:text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """Dynamic inputed text: Python + replace _ for space and show text"""
-    return "Python {}".format(text.replace('_', ' '))
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """Display 'Python' followed by the value of the text variable (default: 'is cool')."""
+    return "Python " + escape(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def only_digits_dynamic(n=None):
-    """Dynamic inputted integer"""
+def number_route(n):
+    """Display 'n is a number' if n is an integer."""
     return "{} is a number".format(n)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
