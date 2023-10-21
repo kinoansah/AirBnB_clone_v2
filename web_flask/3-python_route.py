782e2f53fe1797677b6facc86da7a3df8cb03f67
kinoansah <kinoansah@gmail.com>
2023-10-21 08:14:56 -0700

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""
-Flask web application
+""" Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
-from flask import escape
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Display 'Hello HBNB!' on the root route."""
-    return "Hello HBNB!"
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Display 'HBNB' on the '/hbnb' route."""
-    return "HBNB"
+    """Returns a given string"""
+    return ("HBNB")
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Display 'C' followed by the value of the text variable."""
-    return "C " + escape(text.replace("_", " "))
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    """Display 'Python' followed by the value of the text variable (default: 'is cool')."""
-    return "Python " + escape(text.replace("_", " "))
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
